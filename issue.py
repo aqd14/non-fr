4d74615cfa4561222db1f6dc8659351e42802a9b
Anh Quoc Do <aqd14@msstate.edu>
2017-09-08 14:49:07 -0500

    Add function to save scraped issue to xml file

--- issue.py
+++ issue.py
@@ -1,18 +1,29 @@
 '''
 A class represents for an issue in tracking system
 '''
-from lxml.html.builder import TITLE
 
 class Issue(object):
     """
     
     """
-    def __init__(self, id, title, description, attachments):
-        self.id = id
+    def __init__(self, _id, title, description, attachments):
+        self._id = _id
         self.title = title
         self.description = description
         self.attachments = attachments
     
     def __str__(self):
-        representation = ['id: ' + str(self.id), 'title: ' + self.title, 'description: ' + self.description, 'attachments: ' + self.attachments]
+        representation = ['id: ' + self._id, 'title: ' + self.title, 'description: ' + self.description, 'attachments: ' + self.attachments]
         return '\n'.join(representation)
+    
+    def get_id(self):
+        return self._id
+    
+    def get_title(self):
+        return self.title
+    
+    def get_description(self):
+        return self.description
+    
+    def get_attachments(self):
+        return self.attachments
--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ Simple web scraper for getting data from issue tracking systems
 
 import requests
 from bs4 import BeautifulSoup
+from lxml import etree
 
 import re
 
@@ -12,7 +13,7 @@ from issue import Issue
 FIREFOX_URL_PREFIX = 'https://bugzilla.mozilla.org/show_bug.cgi?id='
 MYLYN_URL_PREFIX = 'https://bugs.eclipse.org/bugs/show_bug.cgi?id='
 
-def scrape_ff(id):
+def scrape_ff(_id):
     """
     Scrape Firefox issues
     """
@@ -23,7 +24,7 @@ def scrape_ff(id):
         
     else:
     '''
-    url = FIREFOX_URL_PREFIX + str(id)
+    url = FIREFOX_URL_PREFIX + str(_id)
     print('Scraping url: %s' % url)
     result = requests.get(url)
     
@@ -41,33 +42,36 @@ def scrape_ff(id):
     description = ''
     
     # Attachments description might reveal some important information about the issue
-    attachments_content = ""
-    attachments = soup.find(id='attachments').find_all(class_='attach-desc')
-    for attach in attachments:
-        attachments_content += attach.a.text + '\n'
+    attachments_content = []
+    attachments = soup.find(id='attachments')
+    if attachments: # if there are some attachments
+        attachments = attachments.find_all(class_='attach-desc')
+        for attach in attachments:
+            attachments_content.append(attach.a.text)
     
     # make an assumption that the reporter will make a more detailed description
     # with the first comment. Consider the first comment as a part of description
     # if the reporter is also the first commenter
-    comments = soup.find_all(class_='change-set')
+    comments = soup.find_all(id=re.compile('^c\d+$'))
     if len(comments) > 0:
         reporter = soup.find(id='field-reporter').find(class_='fna').text
         # print('Reporter: %s' % reporter)
         first_commenter = comments[0].find(class_='fna').text
         # print('First commenter: %s' % first_commenter)
         
-        if reporter is first_commenter:
-            comment = comments[0].find(class_='comment_text').text
+        if first_commenter == reporter: # get issue description if the first commenter is also reporter
+            comment = comments[0].find(class_='comment-text').text
             # print('Comment: %s' % comment)
             description = comment
     
-    return Issue(id, title, description, attachments_content)
+    print('Completed!\n')
+    return Issue(str(_id), title, description, attachments_content)
 
-def scrape_ml(id):
+def scrape_ml(_id):
     """ 
     Scrape Mylyn issues
     """
-    url = MYLYN_URL_PREFIX + str(id)
+    url = MYLYN_URL_PREFIX + str(_id)
     print('Scraping url: %s' % url)
     result = requests.get(url)
     # Can't access the url
@@ -82,11 +86,13 @@ def scrape_ml(id):
     
     # Attachments description might reveal some important information about the issue
     # Include all obsolete attachments
-    attachments_content = ""
-    attachments = soup.find(id='attachment_table').find_all(id=re.compile("^a\d+$"))
-    if len(attachments) > 1:
-        for attach in attachments[1:]:
-            attachments_content += attach.b.text + '\n'
+    attachments_content = []
+    attachments = soup.find(id='attachment_table')
+    if attachments: # if there is attachment
+        attachments = attachments.find_all(id=re.compile("^a\d+$"))
+        if len(attachments) > 1:
+            for attach in attachments[1:]:
+                attachments_content.append(attach.b.text)
     
     # make an assumption that the reporter will make a more detailed description
     # with the first comment. Consider the first comment as a part of description
@@ -98,12 +104,13 @@ def scrape_ml(id):
         first_commenter = comments[0].find(class_='fn').text
         # print('First commenter: %s' % first_commenter)
         
-        if reporter is first_commenter:
+        if first_commenter == reporter: # get issue description if the first commenter is also reporter
             comment = comments[0].find(class_='bz_comment_text').text
             # print('Comment: %s' % comment)
             description = comment
     
-    return Issue(id, title, description, attachments_content)
+    print('Completed!\n')
+    return Issue(str(_id), title, description, attachments_content)
 
 def scrape(system, ids):
     """Scrape a list of issues given the id range.
@@ -121,11 +128,11 @@ def scrape(system, ids):
     system = system.upper()
     # If user want to scrape data within a range of ids
     if len(ids) == 2:
-       for id in range(ids[0], ids[1] + 1):
+        for _id in range(ids[0], ids[1] + 1):
             if system == 'FIREFOX':
-                issue = scrape_ff(id)
+                issue = scrape_ff(_id)
             elif system == 'MYLYN':
-                issue = scrape_ml(id)
+                issue = scrape_ml(_id)
             else:
                 raise RuntimeError('System is unsupported: ' + system)
             issues.append(issue)
@@ -134,11 +141,67 @@ def scrape(system, ids):
     
     return issues
    
-def to_xml(issues):
+def to_xml(f, system, issues):
+    """ Write scraped issues to XML file
+    
+    File format:
+    
+    <root>
+        <system>issue_system</system>
+        <issues>
+            <issue>
+                <id>issue_id</id>
+                <title>issue_title</title>
+                <description>issue_description</description>
+                <attachments>
+                    <attach>attach_description_1</attach>
+                    ....
+                    <attach>attach_description_n</attach>
+                </attachments>
+            </issue>
+            .....
+            <issue>
+                <id>issue_id</id>
+                <title>issue_title</title>
+                <description>issue_description</description>
+                <attachments>
+                    <attach>attach_description_1</attach>
+                    ....
+                    <attach>attach_description_n</attach>
+                </attachments>
+            </issue>
+        </issues>
+    </root>
+    
+    Args:
+        f (file): the file to write
+        system (str): scraped system
+        issues (issue): list of issues
+        
+    Return:
+        XML file
+        
+    """
+    
+    root = etree.Element("root")
+    etree.SubElement(root, "system").text = system
+    issues_e = etree.SubElement(root, "issues")
+    
     for issue in issues:
-        to_xml(issue)
-
-iss_ml = scrape_ml(201154)
-print(iss_ml)
-iss_ff = scrape_ff(200001)
-print(iss_ff)
+        issue_e = etree.SubElement(issues_e, "issue")
+        etree.SubElement(issue_e, "id").text = issue.get_id()
+        etree.SubElement(issue_e, "title").text = issue.get_title()
+        etree.SubElement(issue_e, "description").text = issue.get_description()
+        attachments_e = etree.SubElement(issue_e, "attachments")
+        # add attachment
+        for at in issue.get_attachments():
+            etree.SubElement(attachments_e, "attach").text = at
+    
+    # write to file
+    tree = etree.ElementTree(element=root)
+    tree.write(f, pretty_print=True, xml_declaration=True, encoding='utf-8')
+    
+system = 'Firefox'
+id_range = [200001, 200010]
+issues = scrape(system, id_range)
+to_xml('data/issues.xml', system, issues)
